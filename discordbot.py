760591d4b9ae572ca01afbce37cf2e2f9b2ce784
2339145977 <90003432+2339145977@users.noreply.github.com>
2021-09-03 13:37:33 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,11 +24,6 @@ class MyClient(discord.Client):
             await message.channel.send('https://nekos.life/lewd')
         if message.content.startswith(PREFIX + "晚安"):
             await message.reply('晚安~', mention_author=True)
-            
-@bot.command()
-async def 复读(ctx, arg):
-    await ctx.send(arg)
-
 
 
 token = getenv('DISCORD_BOT_TOKEN')
